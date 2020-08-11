4c383e9c4f8241915e5cf59f8ac76762be9932b8
Patrick Plaisted <patrick.plaisted@gmail.com>
2020-08-10 22:33:03 -0700

    app.py complete!

--- app.py
+++ app.py
@@ -35,8 +35,8 @@ def home():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/start<br/>"
-        f"/api/v1.0/start/end"
+        f"/api/v1.0/&ltstart&gt<br/>"
+        f"/api/v1.0/&ltstart&gt/&ltend&gt"
         )
 
 
@@ -48,6 +48,19 @@ def precipitation():
     '''Convert Query to dict using date as the key and prcp as the value
     Return JSON of dict.'''
 
+    import datetime as dt
+    from dateutil.relativedelta import relativedelta as rd
+    # Query
+    last_date = session.query(func.max(Measurement.date)).first()
+    d = dt.datetime.strptime(last_date[0], '%Y-%m-%d').date()
+    date = (d - rd(years=1)).strftime('%Y-%m-%d')
+
+    q = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date > date).\
+        order_by(Measurement.date).all()
+
+    out = dict(q)
+    return jsonify(out)
 
 @app.route("/api/v1.0/stations")
 def stations():
@@ -69,9 +82,31 @@ def tobs():
     active station for the last year of data.
     Return a JSON list of temperature observations (TOBS) for the previous year.'''
 
+    import datetime as dt
+    from dateutil.relativedelta import relativedelta as rd
+    # Query
+    last_date = session.query(func.max(Measurement.date)).first()
+    d = dt.datetime.strptime(last_date[0], '%Y-%m-%d').date()
+    date = (d - rd(years=1)).strftime('%Y-%m-%d')
 
-@app.route("/api/v1.0/<start>/<end>")
-def start(start, end=''):
+    most_active = session.query(Measurement.station, func.count(Measurement.station)).\
+    group_by(Measurement.station).\
+    order_by(func.count(Measurement.station).desc())
+
+    most_active_station = most_active[0][0]
+
+    most_active_year = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.station == most_active_station).\
+        filter(Measurement.date > date).\
+        group_by(Measurement.date).all()
+
+    tobs = dict(most_active_year)
+    
+    return jsonify(tobs)
+
+
+@app.route("/api/v1.0/<start>")
+def start(start):
     # Create session from Python to the DB
     session = Session(engine)
 
@@ -82,6 +117,47 @@ def start(start, end=''):
         When given the start and the end date, calculate the `TMIN`, `TAVG`, 
     and `TMAX` for dates between the start and end date inclusive.'''
 
+    end='2017-08-23'
+    lowest_temp = session.query(func.min(Measurement.tobs)).\
+        filter(Measurement.date > start).\
+        filter(Measurement.date < end)[0]
+    highest_temp = session.query(func.max(Measurement.tobs)).\
+        filter(Measurement.date > start).\
+        filter(Measurement.date < end)[0]
+    avg_temp = session.query(func.avg(Measurement.tobs)).\
+        filter(Measurement.date > start).\
+        filter(Measurement.date < end)[0]
+
+    listy = [lowest_temp[0], highest_temp[0], avg_temp[0]]
+
+    return jsonify(listy)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_stop(start, end):
+    # Create session from Python to the DB
+    session = Session(engine)
+
+    '''Return a JSON list of the minimum temperature, the average temperature, 
+    and the max temperature for a given start or start-end range.
+        When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for 
+    all dates greater than and equal to the start date.
+        When given the start and the end date, calculate the `TMIN`, `TAVG`, 
+    and `TMAX` for dates between the start and end date inclusive.'''
+
+    start = str(start)
+    lowest_temp = session.query(func.min(Measurement.tobs)).\
+        filter(Measurement.date > start).\
+        filter(Measurement.date < end)[0]
+    highest_temp = session.query(func.max(Measurement.tobs)).\
+        filter(Measurement.date > start).\
+        filter(Measurement.date < end)[0]
+    avg_temp = session.query(func.avg(Measurement.tobs)).\
+        filter(Measurement.date > start).\
+        filter(Measurement.date < end)[0]
+
+    listy = [lowest_temp[0], highest_temp[0], avg_temp[0]]
+
+    return jsonify(listy)
 
 
 @app.route("/magic")
@@ -91,3 +167,11 @@ def magic():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# Debug URLs
+# http://127.0.0.1:5000/api/v1.0/precipitation
+# http://127.0.0.1:5000/api/v1.0/stations
+# http://127.0.0.1:5000/api/v1.0/tobs
+# http://127.0.0.1:5000/api/v1.0/2010-01-01
+# http://127.0.0.1:5000/api/v1.0/2010-01-01/2017-08-23
+# http://127.0.0.1:5000/magic
